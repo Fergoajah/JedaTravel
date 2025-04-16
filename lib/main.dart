960b9622fbf9cdbf59ee694d46481dee25d7d734
fergofernanda <fergofernanda@kali>
2025-04-16 19:21:25 +0700

    Initial commit: upload Flutter project

--- lib/main.dart
+++ lib/main.dart
@@ -3,44 +3,172 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // State untuk menyimpan mode tema
+  bool _isDarkMode = false;
 
   @override
   Widget build(BuildContext context) {
     const String appTitle = 'Flutter layout Fergo';
+
     return MaterialApp(
       title: appTitle,
+      theme: _isDarkMode ? ThemeData.dark() : ThemeData.light(),
       home: Scaffold(
-        appBar: AppBar(title: const Text(appTitle)),
-        body: const SingleChildScrollView(
-        child: Column(
-          children: [
-            ImageSection(
-              image: 'images/lake.jpg',
-            ),
-            TitleSection(
-              name: 'Oeschinen Lake Campground',
-              location: 'Kandersteg, Switzerland',
-            ),
-            ButtonSection(),
-            TextSection(
-              description:
-                  'Lake Oeschinen lies at the foot of the Blüemlisalp in the '
-                  'Bernese Alps. Situated 1,578 meters above sea level, it '
-                  'is one of the larger Alpine Lakes. A gondola ride from '
-                  'Kandersteg, followed by a half-hour walk through pastures '
-                  'and pine forest, leads you to the lake, which warms to 20 '
-                  'degrees Celsius in the summer. Activities enjoyed here '
-                  'include rowing, and riding the summer toboggan run.',
-            ),
+        appBar: AppBar(
+            leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context); // kembali ke halaman sebelumnya
+            },
+          ),
+          title: const Text(appTitle),
+          actions: [
+            IconButton(
+              icon: Icon(
+                _isDarkMode ? Icons.dark_mode : Icons.light_mode,
+              ),
+              onPressed: () {
+                setState(() {
+                  _isDarkMode = !_isDarkMode;
+                });
+              },
+            )
           ],
         ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              ImageSection(
+                image: 'images/lake.jpg',
+              ),
+              TitleSection(
+                name: 'Oeschinen Lake Campground',
+                location: 'Kandersteg, Switzerland',
+              ),
+              ButtonSection(),
+              TextSection(
+                description:
+                    'Lake Oeschinen lies at the foot of the Blüemlisalp in the '
+                    'Bernese Alps. Situated 1,578 meters above sea level, it '
+                    'is one of the larger Alpine Lakes. A gondola ride from '
+                    'Kandersteg, followed by a half-hour walk through pastures '
+                    'and pine forest, leads you to the lake, which warms to 20 '
+                    'degrees Celsius in the summer. Activities enjoyed here '
+                    'include rowing, and riding the summer toboggan run.',
+              ),
+              MorePlacesSection(),
+            ],
+          ),
+        ),
       ),
-      )
     );
   }
 }
+
+class MorePlacesSection extends StatelessWidget {
+  const MorePlacesSection({super.key});
+
+  final List<Map<String, String>> morePlaces = const [
+    {
+      'image': 'images/fumotoppara.jpg',
+      'title': 'Fumotoppara Campground',
+      'description': 'Fumotoppara Campground is a renowned camping destination located in Fujinomiya City, Shizuoka Prefecture, Japan. '
+    },
+    {
+      'image': 'images/Hottarakashi.jpg',
+      'title': 'Hottarakashi Camping Ground',
+      'description': 'Hottarakashi Camping Ground is a scenic campsite located in Yamanashi Prefecture, Japan, at an altitude of approximately 700 meters.'
+    },
+    {
+      'image': 'images/francis.jpg',
+      'title': 'Francis Beach Campground',
+      'description': 'Francis Beach Campground is a coastal camping destination located within Half Moon Bay State Beach in California. It offers 52 campsites suitable for tents, trailers, and RVs up to 40 feet in length.'
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'More Places to Visit',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 8),
+          ListView.builder(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: morePlaces.length,
+            itemBuilder: (context, index) {
+              final place = morePlaces[index];
+              return Material(
+                color: Colors.transparent, // Needed for ripple effect
+                child: InkWell(
+                  onTap: () {
+
+                  },
+                  splashColor: Colors.blue.withOpacity(0.2),
+                  highlightColor: Colors.grey.withOpacity(0.1),
+                  child: Container(
+                    margin: const EdgeInsets.symmetric(vertical: 8),
+                    padding: const EdgeInsets.all(8),
+                    child: Row(
+                      children: [
+                        Image.asset(
+                          place['image']!,
+                          width: 100,
+                          height: 100,
+                          fit: BoxFit.cover,
+                        ),
+                        const SizedBox(width: 12),
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  place['title']!,
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const SizedBox(height: 4),
+                                Text(
+                                  place['description']!,
+                                  style: TextStyle(color: Colors.grey[700]),
+                                  maxLines: 3,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 
 class TitleSection extends StatelessWidget {
   const TitleSection({super.key, required this.name, required this.location});
@@ -83,16 +211,20 @@ class TitleSection extends StatelessWidget {
 class ButtonSection extends StatelessWidget {
   const ButtonSection({super.key});
 
- @override
+  @override
   Widget build(BuildContext context) {
-    final Color color = Theme.of(context).primaryColor;
+    final bool isDarkMode = Theme.of(context).brightness == Brightness.dark;
+
+    final Color iconColor = isDarkMode ? Colors.tealAccent : Theme.of(context).primaryColor;
+
     return SizedBox(
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          ButtonWithText(color: color, icon: Icons.call, label: 'CALL'),
-          ButtonWithText(color: color, icon: Icons.near_me, label: 'ROUTE'),
-          ButtonWithText(color: color, icon: Icons.share, label: 'SHARE'),
+          ButtonWithText(color: iconColor, icon: Icons.call, label: 'CALL',),
+          ButtonWithText(color: iconColor, icon: Icons.near_me, label: 'ROUTE',),
+          ButtonWithText(color: iconColor, icon: Icons.share, label: 'SHARE',),
+          ButtonWithText(color: iconColor, icon: Icons.bookmark, label: 'SAVE',)
         ],
       ),
     );
